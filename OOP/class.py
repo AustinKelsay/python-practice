55af51744d3f3c1050ce0a7a131a2871db33430f
austinkelsay <austinkelsay@yahoo.com>
2021-10-17 10:27:49 -0600

    OOP and classes review

---
+++ OOP/class.py
@@ -0,0 +1,16 @@
+# classes are the blueprints for objects
+# classes are defined by the keyword class
+# objects are instances of a class
+# objects are created by calling a class
+# objects have attributes and methods
+# attributes are variables that store data
+# methods are functions that belong to the class
+
+# create class
+class BigObject:
+    pass
+
+# instantiate class = create an instance of this class aka an object
+obj = BigObject()
+
+print(type(obj))
